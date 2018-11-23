420aa393108e7c61b5d4de47732fb0ef2c4f8ef7
MyMac <benjaminsbrown98@gmail.com>
2018-11-23 17:50:07 +0000

    signup

---
+++ coursework2/cw.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+	return render_template('home.html'), 200
+@app.route('/signup')
+def root():
+	return render_template('signup.html'), 200
