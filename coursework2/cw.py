3820c46719324f5a6331d5d4146b8ebfa0fe5659
MyMac <benjaminsbrown98@gmail.com>
2018-11-24 23:19:21 +0000

    .

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+    def login():
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+                error = 'Invalid Username and/or Password please try again.'
+            else:
+                session['Logged_in'] = True
+                return redirect(url_for('home'))
+                return render_template('login.html', error=error)
+@app.route('logout')
+    def logout():
+        session.pop('Logged_in', None)
 @app.route('/home')
-def home():
-	return render_template('home.html'), 200
+    def home():
+	       return render_template('home.html'), 200
 @app.route('/signup')
-def signup():
-	return render_template('signup.html'), 200
+    def signup():
+	       return render_template('signup.html'), 200
