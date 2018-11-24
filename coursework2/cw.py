9deb7b44817bb737a8b81db8c305286b4e114960
MyMac <benjaminsbrown98@gmail.com>
2018-11-24 22:58:44 +0000

    login check for admin

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('/'))
+    return render_template('login.html', error=error)
 @app.route('/')
 def root():
 	return render_template('home.html'), 200
 @app.route('/signup')
 def signup():
 	return render_template('signup.html'), 200
-@app.route('/login')
-def login():
-    return render_template('login.html'), 200
