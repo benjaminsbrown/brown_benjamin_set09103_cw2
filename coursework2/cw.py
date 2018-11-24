1dc2fd7f129a77c8a16d2fc242b4d6b95c09cddd
MyMac <benjaminsbrown98@gmail.com>
2018-11-24 23:23:47 +0000

    add session

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
