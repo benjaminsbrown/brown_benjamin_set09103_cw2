65be328cb9da8d11efe12c820a94af26251b49c2
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 02:08:34 +0000

    idek

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -3,23 +3,23 @@ app = Flask(__name__)
 app.secret_key = "super secret"
 
 @app.route('/login', methods=['GET', 'POST'])
-def home():
+def login():
         error = None
         if request.method == 'POST':
             if request.form['username'] != 'admin' or request.form['password'] != 'admin':
                 error = 'Invalid Username and/or Password please try again.'
             else:
                 session['Logged_in'] = True
-                return redirect(url_for('main'))
+                return redirect(url_for('home'))
             return render_template('home.html', error=error)
 
 @app.route('/logout')
 def logout():
         session.pop('Logged_in', None)
-        return redirect(url_for('main'))
+        return redirect(url_for('home'))
 
 @app.route('/')
-def main():
+def home():
 	       return render_template('home.html'), 200
 @app.route('/signup')
 def signup():
