70c705a79390b180e5dafd31cc4920aeed0a46cc
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 22:44:37 +0000

    .

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect, session, flash, g, SQLAlchemy
-from flask.ext.sqlalchemy import
+from flask import Flask, render_template, url_for, request, redirect, session, flash, g
+from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 
 # import sqlite3
