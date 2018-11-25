458dbc0cd141049e805597c54eaf19aff257e480
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 01:54:01 +0000

    fallback

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
         error = None
@@ -8,19 +7,15 @@ def login():
             if request.form['username'] != 'admin' or request.form['password'] != 'admin':
                 error = 'Invalid Username and/or Password please try again.'
             else:
-                session['logged_in'] = True
+                session['Logged_in'] = True
                 return redirect(url_for('home'))
-            return render_template('login.html', error=error)
-
+                return render_template('login.html', error=error)
 @app.route('/logout')
 def logout():
-        session.pop('logged_in', None)
-        return redirect(url_for('home'))
-
+        session.pop('Logged_in', None)
 @app.route('/home')
 def home():
 	       return render_template('home.html'), 200
-
 @app.route('/signup')
 def signup():
 	       return render_template('signup.html'), 200
