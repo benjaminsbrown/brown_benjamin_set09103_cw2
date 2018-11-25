7140a738557f522ab593fcc40e61ba05af1b707f
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 01:37:14 +0000

    secret key

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 app = Flask(__name__)
+app.secret_key= "super secret"
 @app.route('/', methods=['GET', 'POST'])
 def login():
         error = None
