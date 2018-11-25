d47611473656b7bba54e34d3af9198ad6e8daaf7
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 01:43:34 +0000

    bug fix

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -8,16 +8,19 @@ def login():
             if request.form['username'] != 'admin' or request.form['password'] != 'admin':
                 error = 'Invalid Username and/or Password please try again.'
             else:
-                session['Logged_in'] = True
+                session['logged_in'] = True
                 return redirect(url_for('home'))
                 return render_template('login.html', error=error)
+
 @app.route('/logout')
 def logout():
-        session.pop('Logged_in', None)
+        session.pop('logged_in', None)
         return redirect(url_for('home'))
+        return render_template('login.html'), 200
 @app.route('/home')
 def home():
 	       return render_template('home.html'), 200
+
 @app.route('/signup')
 def signup():
 	       return render_template('signup.html'), 200
