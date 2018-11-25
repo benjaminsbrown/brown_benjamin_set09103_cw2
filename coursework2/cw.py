85891875fc7cabbd825dc696ad36165b1b6bc533
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 22:40:43 +0000

    test

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, session, flash, g
 
 from functools import wraps
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import SQLAlchemy
 # import sqlite3
 
 app = Flask(__name__)
