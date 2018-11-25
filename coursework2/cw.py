fa708c14e1d7b30c926199180de59d61818bef3d
MyMac <benjaminsbrown98@gmail.com>
2018-11-25 02:40:05 +0000

    verify logged in

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, url_for, request, redirect, session, f
 app = Flask(__name__)
 app.secret_key = "super secret"
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'Logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login ya snake!')
+            return redirect(url_for('login'))
+        return wrap
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
         error = None
@@ -10,17 +20,18 @@ def login():
                 error = 'Invalid Username and/or Password please try again.'
             else:
                 session['Logged_in'] = True
-                flash('You weer just logged in!')
+                flash('You were just logged in!')
                 return redirect(url_for('home'))
         return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
         session.pop('Logged_in', None)
-        flash('You weer just logged out!')
+        flash('You were just logged out!')
         return redirect(url_for('home'))
 
 @app.route('/')
+@login_required
 def home():
 	    return render_template('home.html'), 200
 @app.route('/signup')
