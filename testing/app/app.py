283ddb8febcf49766da7dc484ac1382979e81201
MyMac <benjaminsbrown98@gmail.com>
2018-11-26 14:32:20 +0000

    .

--- testing/app/app.py
+++ testing/app/app.py
@@ -80,5 +80,126 @@ def main():
     database.create_tables([Entry, FTSEntry])
     app.run(debug=True)
 
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
+@app.route('/')
+def index():
+    search_query = request.args.get('q')
+    if search_query:
+        query = Entry.search(search_query)
+    else:
+        query = Entry.public().order_by(Entry.timestamp.desc())
+    return object_list('index.html', query, search=search_query)
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        if password == app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('login.html', next_url=next_url)
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('logout.html')
+
+@classmethod
+def public(cls):
+    return Entry.select().where(Entry.published == True)
+
+@classmethod
+def search(cls, query):
+    words = [word.strip() for word in query.split() if word.strip()]
+    if not words:
+        # Return empty query.
+        return Entry.select().where(Entry.id == 0)
+    else:
+        search = ' '.join(words)
+
+    return (Entry
+            .select(Entry, FTSEntry.rank().alias('score'))
+            .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
+            .where(
+                (Entry.published == True) &
+                (FTSEntry.match(search)))
+            .order_by(SQL('score')))
+@classmethod
+def drafts(cls):
+    return Entry.select().where(Entry.published == False)
+@app.route('/drafts/')
+@login_required
+def drafts():
+    query = Entry.drafts().order_by(Entry.timestamp.desc())
+    return object_list('index.html', query)
+@app.route('/<slug>/')
+def detail(slug):
+    if session.get('logged_in'):
+        query = Entry.select()
+    else:
+        query = Entry.public()
+    entry = get_object_or_404(query, Entry.slug == slug)
+    return render_template('detail.html', entry=entry)
+@property
+def html_content(self):
+    hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
+    extras = ExtraExtension()
+    markdown_content = markdown(self.content, extensions=[hilite, extras])
+    oembed_content = parse_html(
+        markdown_content,
+        oembed_providers,
+        urlize_all=True,
+        maxwidth=app.config['SITE_WIDTH'])
+    return Markup(oembed_content)
+@app.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            entry = Entry.create(
+                title=request.form['title'],
+                content=request.form['content'],
+                published=request.form.get('published') or False)
+            flash('Entry created successfully.', 'success')
+            if entry.published:
+                return redirect(url_for('detail', slug=entry.slug))
+            else:
+                return redirect(url_for('edit', slug=entry.slug))
+        else:
+            flash('Title and Content are required.', 'danger')
+    return render_template('create.html')
+@app.route('/<slug>/edit/', methods=['GET', 'POST'])
+@login_required
+def edit(slug):
+    entry = get_object_or_404(Entry, Entry.slug == slug)
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            entry.title = request.form['title']
+            entry.content = request.form['content']
+            entry.published = request.form.get('published') or False
+            entry.save()
+
+            flash('Entry saved successfully.', 'success')
+            if entry.published:
+                return redirect(url_for('detail', slug=entry.slug))
+            else:
+                return redirect(url_for('edit', slug=entry.slug))
+        else:
+            flash('Title and Content are required.', 'danger')
+
+    return render_template('edit.html', entry=entry)
 if __name__ == '__main__':
     main()
