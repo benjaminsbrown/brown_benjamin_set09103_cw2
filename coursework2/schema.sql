12a045069e959c2d8108676e8b64136fcefe66b6
MyMac <benjaminsbrown98@gmail.com>
2018-11-29 18:23:20 +0000

    .

--- coursework2/schema.sql
+++ coursework2/schema.sql
@@ -1,6 +1,6 @@
 drop TABLE if exists entries;
 create table entries (
   ID integer primary key autoincrement,
-  TITLE string not null,
-  TEXT string not null
+  title string not null,
+  text string not null
 );
