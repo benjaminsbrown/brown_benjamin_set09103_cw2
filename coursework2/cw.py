73c488b5dd2ea44dfe266c79eeb201a684b4e3a9
MyMac <benjaminsbrown98@gmail.com>
2018-11-29 17:53:11 +0000

    non ascii character

--- coursework2/cw.py
+++ coursework2/cw.py
@@ -77,7 +77,7 @@ def home():
     g.db = connect_db()
     cur = g.db.execute('SELECT title, text from entries order by id desc')
     entries = [dict(title = row[0], text=row[1]) for row in cur.fetchall()]
-    return  render_template ( 'show_entries.html' ,  entries = entries)
+    return  render_template ( 'show_entries.html', entries=entries)
 
 @app.route('/signup')
 def signup():
@@ -85,7 +85,7 @@ def signup():
 @app.route('/add', methods=['POST'])
 @login_required
 def add_entry():
-    g.db.execute('INSERT INTO entries (title,text)values ​​(?, ?)',[request.form['TITLE'], request.form['TEXT']])
+    g.db.execute('INSERT INTO entries (title,text)values ',[request.form['TITLE'], request.form['TEXT']])
     g.db.commit()
     flash('New entry was added')
     return redirect(url_for('home'))
